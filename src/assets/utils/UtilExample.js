12084951a725a024ed5a98fc1ba27d13f1b06db3
Kevin Parra <kevinparra535@gmail.com>
2022-08-18 11:55:19 -0500

    [ADD] STACK INICIAL

---
+++ src/assets/utils/UtilExample.js
@@ -0,0 +1,11 @@
+/**
+ * Descripcion: Para que funciona este helper
+ * Bugs: 0.
+ */
+
+const example = async () => {
+  console.log(`%c Helper example`, 'font-weight:bold');
+
+};
+
+export default example;
